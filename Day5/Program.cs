1daf29567058ba96c38e41886f4b763e67408ad3
ronmeher <ronmeher@gmail.com>
2022-06-06 06:39:39 +0530

    Initial Commit

---
+++ Day5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 5 Problems");
+        }
+    }
+}
